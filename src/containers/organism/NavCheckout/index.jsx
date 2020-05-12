34258226c4c3be1af6b3df6929ebd5ed2c4fc048
andinaeta <mikorlk4328@gmail.com>
2020-05-12 20:27:01 +0700

    framer-motion implement

--- src/containers/organism/NavCheckout/index.jsx
+++ src/containers/organism/NavCheckout/index.jsx
@@ -1,31 +1,63 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './index.css';
 import Navbar from 'react-bootstrap/Navbar';
 import { NavLink } from 'react-router-dom';
 import troleyicon from '../../../assets/img/icon/troley-icon.svg';
 import icon from '../../../assets/img/icon/move-to-icon.svg';
 import { connect } from 'react-redux';
+import { motion } from 'framer-motion';
+
+function Nav({ addedItems, history }) {
+    const [shouldShowActions, setShouldShowActions] = useState(true);
+    const [lastYPos, setLastYPos] = useState(0);
+
+    useEffect(() => {
+        function handleScroll() {
+          const yPos = window.scrollY;
+          const isScrollingUp = yPos < lastYPos;
+
+          setShouldShowActions(isScrollingUp);
+          setLastYPos(yPos);
+        }
+
+        window.addEventListener('scroll', handleScroll, false);
+
+        return () => {
+          window.removeEventListener('scroll', handleScroll, false);
+        }
+    }, [lastYPos]);
+    return (
+        <motion.div
+            animate={{ opacity: shouldShowActions ? 1 : 0 }}
+            initial={{ opacity: 0 }}
+            transition={{ opacity: { duration: 0.2 } }}
+        >
+            <Navbar
+                className="nav-checkout"
+                style={{ cursor: "pointer" }}
+                onClick={() => history.push('/shoppingcart')}
+            >
+                <div className="go-checkout un-text-d_">
+                    <img src={troleyicon} alt="icon" className="troley-icon" />
+                    <div className="text">
+                        <span className="counter">{addedItems.length}</span>
+                    </div>
+                    <NavLink to="/shoppingcart">
+                        <img src={icon} alt="icon" className="icon" />
+                    </NavLink>
+                </div>
+            </Navbar>
+        </motion.div>
+    );
+}
 
 class NavCheckout extends React.Component {
-
     render() {
-        const addedItems = this.props.addedItems;
+        const { addedItems } = this.props;
         return (
-            addedItems.length
-                ?
-                <Navbar className="nav-checkout">
-                    <div className="go-checkout un-text-d_">
-                        <img src={troleyicon} alt="icon" className="troley-icon" />
-                        <div className="text">
-                            Lihat isi Keranjang
-                            <span className="counter">{addedItems.length}</span>
-                        </div>
-                        <NavLink to="/shoppingcart">
-                            <img src={icon} alt="icon" className="icon" />
-                        </NavLink>
-                    </div>
-                </Navbar>
-                : <></>
+            addedItems.length ? (
+                <Nav {...this.props} />
+            ) : null
         );
     }
 }
